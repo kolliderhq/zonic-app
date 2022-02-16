49a0212272c5d49c799ec92b8faa470baf871502
the-kollider <info@kollider.xyz>
2022-02-16 22:46:33 +0000

    handled the case where wallets are not loaded

--- components/ConvertFunds.js
+++ components/ConvertFunds.js
@@ -14,6 +14,7 @@ import { roundDecimal } from 'utils/format';
 import { WrapBaseDialog } from './dialogs';
 import { ConfirmConversion } from './dialogs/ConfirmConversion';
 import { displayToast, TOAST_LEVEL } from 'utils/toast';
+import Loader from './Loader';
 
 export const ConvertFunds = () => {
 	let dispatch = useAppDispatch();
@@ -27,6 +28,7 @@ export const ConvertFunds = () => {
 	const [fees, setFees] = useState(0);
 	const [contractValue, setContractValue] = useState(0);
 	const [showConversionConfirmation, setShowConversionConfirmation] = useState(false);
+	const [walletsHaveLoaded, setWalletsHaveLoaded] = useState(false)
 
 	const onChangeBuyUSD = isBuyUSD => {
 		setIsBuying(isBuyUSD);
@@ -57,6 +59,11 @@ export const ConvertFunds = () => {
 			setShowConversionConfirmation(true)
 		}
 	};
+
+	useEffect(() => {
+		if (!wallets["USD"] || !wallets["BTC"]) return
+		setWalletsHaveLoaded(true)
+	}, [wallets])
 
 	useEffect(() => {
 		if (isNaN(parseInt(dollarConversionAmount))) {
@@ -108,75 +115,81 @@ export const ConvertFunds = () => {
 								Sell USD
 							</div>
 						</div>
-						<div className="h-full flex flex-col">
-							<div className="m-auto">
-								<div className="mt-5 flex grid grid-cols-2 divide-x">
-									<div className="text-left pl-4">
-										{isBuying ? (
-											<div className="w-full">
-												BTC balance:{' '}
-												<div className="text-xl text-gray-600 w-full">
-													{roundDecimal(wallets['BTC'].balance, wallets['BTC'].dp) / 100000000}
+						{
+							walletsHaveLoaded ? (
+								<Loader color={'#7372f7'} />
+							) : (
+								<div className="h-full flex flex-col">
+									<div className="m-auto">
+										<div className="mt-5 flex grid grid-cols-2 divide-x">
+											<div className="text-left pl-4">
+												{isBuying ? (
+													<div className="w-full">
+														BTC balance:{' '}
+														<div className="text-xl text-gray-600 w-full">
+															{roundDecimal(wallets['BTC'].balance, wallets['BTC'].dp) / 100000000}
+														</div>
+													</div>
+												) : (
+													<div className="w-full">
+														USD balance:{' '}
+														<div className="text-xl text-gray-600 w-full">
+															{roundDecimal(wallets['USD'].balance, wallets['USD'].dp)}
+														</div>
+													</div>
+												)}
+											</div>
+											<div className="text-left pl-4">
+												<div>Bitcoin Price: </div>
+												<div className="text-xl text-gray-600">${markPrice}</div>
+											</div>
+										</div>
+										<div className="mt-4">
+											<div>
+												<div className="flex text-lg">
+													How much USD do you want to{' '}
+													<span className="ml-2 text-gray-700">{isBuying ? 'buy' : 'sell'}</span>?
+												</div>
+												<div className="flex rounded-md border-2 border-gray-300 focus:border-gray-300 hover:border-gray-300 mt-4 p-2 text-gray-600">
+													<input
+														// onFocus={() => console.log('}
+														className="input-default inline-block w-full h-12 text-2xl"
+														type="number"
+														onInput={e => setDollarConversionAmount(e.target.value)}
+													// value={}
+													/>
+													<div className="m-auto text-2xl border-l-2 pl-2 text-gray-600"> USD </div>
+												</div>
+												<div className="text-red-500">
+													{!inputIsValid && <p className="text-sm">Input must be an integer.</p>}
 												</div>
 											</div>
-										) : (
-											<div className="w-full">
-												USD balance:{' '}
-												<div className="text-xl text-gray-600 w-full">
-													{roundDecimal(wallets['USD'].balance, wallets['USD'].dp)}
-												</div>
+										</div>
+										<div className="mt-2">
+											<div className="w-full text-left text-lg">
+												{isBuying ? <div>You will have to pay </div> : <div>You will get </div>}
 											</div>
-										)}
-									</div>
-									<div className="text-left pl-4">
-										<div>Bitcoin Price: </div>
-										<div className="text-xl text-gray-600">${markPrice}</div>
-									</div>
-								</div>
-								<div className="mt-4">
-									<div>
-										<div className="flex text-lg">
-											How much USD do you want to{' '}
-											<span className="ml-2 text-gray-700">{isBuying ? 'buy' : 'sell'}</span>?
+											<div className="text-3xl">
+												{contractValue} <span className="text-lg">SATS</span>
+											</div>
 										</div>
-										<div className="flex rounded-md border-2 border-gray-300 focus:border-gray-300 hover:border-gray-300 mt-4 p-2 text-gray-600">
-											<input
-												// onFocus={() => console.log('}
-												className="input-default inline-block w-full h-12 text-2xl"
-												type="number"
-												onInput={e => setDollarConversionAmount(e.target.value)}
-												// value={}
-											/>
-											<div className="m-auto text-2xl border-l-2 pl-2 text-gray-600"> USD </div>
+										<div className="mt-4">
+											<div className="">Fees:</div>
+											<div className="text-xl">{roundDecimal(fees, 0)} Sats</div>
 										</div>
-										<div className="text-red-500">
-											{!inputIsValid && <p className="text-sm">Input must be an integer.</p>}
+									</div>
+									<div className="h-full relative">
+										<div className="mt-4 bottom-0 absolute w-full">
+											<button
+												className="border border-theme-main px-4 py-2 rounded-lg border-2 text-theme-main font-bold w-full"
+												onClick={() => onMakeConversion()}>
+												Convert
+											</button>
 										</div>
 									</div>
 								</div>
-								<div className="mt-2">
-									<div className="w-full text-left text-lg">
-										{isBuying ? <div>You will have to pay </div> : <div>You will get </div>}
-									</div>
-									<div className="text-3xl">
-										{contractValue} <span className="text-lg">SATS</span>
-									</div>
-								</div>
-								<div className="mt-4">
-									<div className="">Fees:</div>
-									<div className="text-xl">{roundDecimal(fees, 0)} Sats</div>
-								</div>
-							</div>
-							<div className="h-full relative">
-								<div className="mt-4 bottom-0 absolute w-full">
-									<button
-										className="border border-theme-main px-4 py-2 rounded-lg border-2 text-theme-main font-bold w-full"
-										onClick={() => onMakeConversion()}>
-										Convert
-									</button>
-								</div>
-							</div>
-						</div>
+							)
+						}
 					</div>
 				</div>
 			)}
